992803aa853a4ec8c71512bd9b2c87d6c02b033d
nesmaabdou <nesmasharief@gmail.com>
2024-02-29 14:45:08 +0100

    added functionality to update conditions, humidity and wind speed through api

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ function updateWeather (response) {
 let tempElement = document.querySelector("#temp");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#codition-description")
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
 
+cityElement.innerHTML= response.data.city;
 tempElement.innerHTML = Math.round (temperature);
-cityElement.innerHTML= response.data.city;;
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity (city) {
@@ -23,5 +29,5 @@ function submitSearch(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
 
-searchCity("Cairo");
+searchCity("Cairo"); 
 
